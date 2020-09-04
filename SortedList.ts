ddfd93e4df8bbeb8f8f92c186403eb2efab9d4e1
Frank Mayer <frank.mayer19072001@outlook.de>
2020-09-04 22:04:06 +0200

    added my existing classes and functions

---
+++ SortedList.ts
@@ -0,0 +1,41 @@
+class SortedList<T> {
+  private list: Array<T>;
+  constructor() {
+    this.list = new Array<T>();
+  }
+  add(value: T) {
+    this.list.push(value);
+    this.list.sort();
+  }
+  remove(value: T) {
+    this.list.splice(this.indexOf(value), 1);
+  }
+  indexOf(value: T): number {
+    let firstIndex = 0,
+      lastIndex = this.list.length - 1,
+      middleIndex = Math.floor((lastIndex + firstIndex) / 2);
+    while (this.list[middleIndex] !== value && firstIndex < lastIndex) {
+      if (value < this.list[middleIndex]) {
+        lastIndex = middleIndex - 1;
+      } else if (value > this.list[middleIndex]) {
+        firstIndex = middleIndex + 1;
+      }
+      middleIndex = Math.floor((lastIndex + firstIndex) / 2);
+    }
+    return this.list[middleIndex] !== value ? -1 : middleIndex;
+  }
+  includes(value: T): boolean {
+    let firstIndex = 0,
+      lastIndex = this.list.length - 1,
+      middleIndex = Math.floor((lastIndex + firstIndex) / 2);
+    while (this.list[middleIndex] !== value && firstIndex < lastIndex) {
+      if (value < this.list[middleIndex]) {
+        lastIndex = middleIndex - 1;
+      } else if (value > this.list[middleIndex]) {
+        firstIndex = middleIndex + 1;
+      }
+      middleIndex = Math.floor((lastIndex + firstIndex) / 2);
+    }
+    return this.list[middleIndex] === value;
+  }
+}
