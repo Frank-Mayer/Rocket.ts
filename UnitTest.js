f2e0f7e772307b98cab7a72e52a509eb101eb415
Frank Mayer <frank.mayer19072001@outlook.de>
2020-09-09 22:56:22 +0200

    Added more unuttests

--- UnitTest.js
+++ UnitTest.js
@@ -1,4 +1,4 @@
-import { HashTable, SortedList } from "./out/speedup.js";
+import { HashTable, SortedList, Stack, Quere } from "./out/speedup.js";
 
 const test = (operation, expectation) => {
   let result;
@@ -21,3 +21,25 @@ test(hashTable.get("meep"), "12345");
 test(hashTable.length, 3);
 test(hashTable.pop(33), 4204);
 test(hashTable.length, 2);
+
+const sortedList = new SortedList();
+sortedList.add("tux");
+sortedList.add("seehorse");
+sortedList.add("apple");
+test(sortedList.remove("seehorse"), true);
+test(sortedList.remove("bear"), false);
+test(sortedList.indexOf("apple"), 0);
+
+const stack = new Stack();
+stack.push("meep");
+test(stack.peek(), "meep");
+test(stack.pop(), "meep");
+test(stack.length, 0);
+
+const quere = new Quere();
+quere.enqueue("dog");
+quere.enqueue("cat");
+quere.enqueue("bird");
+test(quere.dequeue(), "dog");
+test(quere.dequeue(), "cat");
+test(quere.dequeue(), "bird");
