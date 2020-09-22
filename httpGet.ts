acfcbdf1cf816050c4efc70c036a67da1b4b1cd3
Frank Mayer <frank.mayer19072001@outlook.de>
2020-09-22 07:45:19 +0200

    added cache to httpGet

--- httpGet.ts
+++ httpGet.ts
@@ -1,12 +1,30 @@
-async function httpGet(url: string) {
-  return new Promise(function (resolve: (value: string) => void) {
+const httpGetCache = new HashTable(128);
+async function httpGet(url: string, cached: boolean = false) {
+  return new Promise(function (
+    resolve: (value: string) => void,
+    reject: (reason?: any) => void
+  ) {
+    if (cached && httpGetCache.has(url)) {
+      let cache = httpGetCache.get(url);
+      if (cache[0]) {
+        resolve(cache[1]);
+      } else {
+        reject(cache[1]);
+      }
+    }
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = () => {
       if (xmlHttp.readyState == 4) {
         if (xmlHttp.status == 200) {
+          if (cached) {
+            httpGetCache.set(url, [true, xmlHttp.responseText]);
+          }
           resolve(xmlHttp.responseText);
         } else {
-          console.error(xmlHttp.response);
+          if (cached) {
+            httpGetCache.set(url, [false, xmlHttp.response]);
+          }
+          reject(xmlHttp.response);
         }
       }
     };
