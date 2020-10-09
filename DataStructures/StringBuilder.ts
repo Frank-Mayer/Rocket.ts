9466b05cc51a9b7dd781a4166a3f15d9cc56436d
Frank Mayer <frank.mayer19072001@outlook.de>
2020-10-09 23:33:05 +0200

    StringBuilder implemented

---
+++ DataStructures/StringBuilder.ts
@@ -0,0 +1,46 @@
+class StringBuilder {
+  private bucket: Array<string>;
+  public length: number;
+
+  constructor(value: string | undefined = undefined) {
+    this.bucket = new Array<string>();
+    if (value) {
+      this.bucket.push(value);
+      this.length = value.length;
+    } else {
+      this.length = 0;
+    }
+  }
+
+  [Symbol.iterator]() {
+    return this.toString().split("");
+  }
+
+  toString() {
+    return this.bucket.join("");
+  }
+
+  append(value: string) {
+    if (value) {
+      this.bucket.push(value);
+      this.length += value.length;
+    }
+  }
+
+  clear() {
+    this.length = 0;
+  }
+
+  replace(searchValue: string | RegExp, replaceValue: string) {
+    this.bucket = [this.toString().replace(searchValue, replaceValue)];
+    this.countLength();
+  }
+
+  private async countLength() {
+    let c = 0;
+    for await (const el of this.bucket) {
+      c += el.length;
+    }
+    this.length = c;
+  }
+}
