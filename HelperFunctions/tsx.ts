a4d03028b3d11b9cea6ae3b2201553527a6ab91d
Frank Mayer <frank.mayer19072001@outlook.de>
2020-12-26 15:39:53 +0100

    fixed tsx

--- HelperFunctions/tsx.ts
+++ HelperFunctions/tsx.ts
@@ -1,11 +1,38 @@
-function tsx(html: string): HTMLElement | null {
-  try {
-    html = html.trim();
-    let tagName = html.split("<")[1].split(" ")[0];
-    let x = document.createElement(tagName);
-    x.outerHTML = html;
-    return x;
-  } catch {
-    return null;
+function tsx<T extends keyof HTMLElementTagNameMap>(
+  tagName: T,
+  properties?: { [key: string]: string | boolean | number }
+): HTMLElementTagNameMap[T] {
+  const x = document.createElement(tagName);
+  if (properties) {
+    const attr = (key: string, value: string | boolean | number): void => {
+      switch (typeof value) {
+        case "string":
+          x.setAttribute(key, value);
+          break;
+        case "number":
+          x.setAttribute(key, value.toString());
+          break;
+        case "boolean":
+          if (value) {
+            x.setAttribute(key, key);
+          } else {
+            x.removeAttribute(key);
+          }
+          break;
+      }
+    };
+    for (const key of Object.keys(properties)) {
+      const value = properties[key];
+      try {
+        if (key.startsWith("on")) {
+          attr(key, value);
+        } else {
+          new Function("x", "v", `x.${key} = v`)(x, value);
+        }
+      } catch {
+        attr(key, value);
+      }
+    }
   }
+  return x;
 }
