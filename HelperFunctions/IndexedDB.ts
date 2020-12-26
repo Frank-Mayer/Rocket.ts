4d5b2868044df0085b9af8f2aaa49681451f64a2
Frank Mayer <frank.mayer19072001@outlook.de>
2020-12-26 15:38:49 +0100

    Typed IDB

--- HelperFunctions/IndexedDB.ts
+++ HelperFunctions/IndexedDB.ts
@@ -1,15 +1,57 @@
-class IDB {
+class IDB<T> {
   protected readonly dbName: string;
-  constructor(dbName: string) {
+  protected readonly dbVersion: number;
+  constructor(dbName: string, version: number = 1) {
     this.dbName = dbName;
+    this.dbVersion = version;
   }
-  get<T>(id: string): Promise<T> {
+  get(id: string): Promise<T> {
     const storeName = this.dbName;
+    const storeVersion = this.dbVersion;
     return new Promise(function (resolve, reject) {
-      var dbRequest = indexedDB.open(storeName);
+      var dbRequest = indexedDB.open(storeName, storeVersion);
 
-      dbRequest.onerror = function () {
-        reject(Error("Error text"));
+      dbRequest.onerror = function (e) {
+        reject(e);
+      };
+
+      dbRequest.onupgradeneeded = function (event) {
+        // Objectstore does not exist. Nothing to load
+        (<any>event.target).transaction.abort();
+        reject(Error("Not found"));
+      };
+
+      dbRequest.onsuccess = function (event) {
+        const database = (<any>event.target).result;
+        const transaction = database.transaction([storeName]);
+        const objectStore = transaction.objectStore(storeName);
+        const objectRequest = <IDBRequest<IDBCursorWithValue>>(
+          objectStore.get(id)
+        );
+
+        objectRequest.onerror = function (e) {
+          reject(e);
+        };
+
+        objectRequest.onsuccess = function () {
+          if (objectRequest.result) {
+            resolve(objectRequest.result.value);
+          } else reject(Error("object not found"));
+        };
+      };
+    });
+  }
+  select(
+    callback: (cursor: IDBCursorWithValue) => void
+    // ,where?: string
+  ): Promise<Array<T>> {
+    const storeName = this.dbName;
+    const storeVersion = this.dbVersion;
+    return new Promise(function (resolve, reject) {
+      const dbRequest = indexedDB.open(storeName, storeVersion);
+
+      dbRequest.onerror = function (e) {
+        reject(e);
       };
 
       dbRequest.onupgradeneeded = function (event) {
@@ -22,29 +64,75 @@ class IDB {
         var database = (<any>event.target).result;
         var transaction = database.transaction([storeName]);
         var objectStore = transaction.objectStore(storeName);
-        var objectRequest = objectStore.get(id);
+        const objectRequest = <IDBRequest<IDBCursorWithValue>>(
+          objectStore.openCursor()
+        );
 
         objectRequest.onerror = function () {
-          reject(Error("Error text"));
+          // reject(e);
         };
 
-        objectRequest.onsuccess = function () {
-          if (objectRequest.result) resolve(objectRequest.result.value);
-          else reject(Error("object not found"));
+        objectRequest.onsuccess = function (ev) {
+          if (objectRequest.result) {
+            const cursor = <IDBCursorWithValue>(<any>ev.target).result;
+            if (cursor) {
+              callback(cursor.value);
+              resolve(cursor.value);
+              cursor.continue();
+            }
+          }
+          // else {
+          //   reject(Error("object not found"));
+          // }
         };
       };
     });
   }
-  set(id: string, value: any): Promise<void> {
+  delete(id: string): Promise<boolean> {
+    const storeName = this.dbName;
+    const storeVersion = this.dbVersion;
+    return new Promise(function (resolve, reject) {
+      var dbRequest = indexedDB.open(storeName, storeVersion);
+
+      dbRequest.onerror = function (e) {
+        reject(e);
+      };
+
+      dbRequest.onupgradeneeded = function (event) {
+        // Objectstore does not exist. Nothing to load
+        (<any>event.target).transaction.abort();
+        reject(Error("Not found"));
+      };
+
+      dbRequest.onsuccess = function (event) {
+        var database = (<any>event.target).result;
+        var transaction = database.transaction([storeName], "readwrite");
+        var objectStore = transaction.objectStore(storeName);
+        var objectRequest = <IDBRequest<IDBCursorWithValue>>(
+          objectStore.delete(id)
+        );
+
+        objectRequest.onerror = function () {
+          resolve(false);
+        };
+
+        objectRequest.onsuccess = function () {
+          resolve(true);
+        };
+      };
+    });
+  }
+  set(id: string, value: T): Promise<void> {
     const object = JSON.parse(
       `{"id":"${id}","value":${JSON.stringify(value)}}`
     );
     const storeName = this.dbName;
+    const storeVersion = this.dbVersion;
     return new Promise(function (resolve, reject) {
       if (!object.id) {
         reject(Error("object has no id."));
       }
-      var dbRequest = indexedDB.open(storeName);
+      var dbRequest = indexedDB.open(storeName, storeVersion);
 
       dbRequest.onerror = function () {
         reject(Error("IndexedDB database error"));
@@ -61,16 +149,28 @@ class IDB {
         var database = (<any>event.target).result;
         var transaction = database.transaction([storeName], "readwrite");
         var objectStore = transaction.objectStore(storeName);
-        var objectRequest = objectStore.put(object); // Overwrite if exists
+        var objectRequest = <IDBRequest<IDBCursorWithValue>>(
+          objectStore.put(object)
+        ); // Overwrite if exists
 
-        objectRequest.onerror = function () {
-          reject(Error("Error text"));
+        objectRequest.onerror = function (e) {
+          reject(e);
         };
 
         objectRequest.onsuccess = function () {
           resolve();
         };
       };
+    });
+  }
+  update<K extends keyof T, V extends T[K]>(id: string, key: K, newValue: V) {
+    return new Promise((resolve, reject) => {
+      this.get(id)
+        .then((obj) => {
+          obj[key] = newValue;
+          this.set(id, obj).then(resolve).catch(reject);
+        })
+        .catch(reject);
     });
   }
 }
