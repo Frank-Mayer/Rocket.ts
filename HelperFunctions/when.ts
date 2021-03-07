572bb5493432fcb9d177b21a3ef002adafffdfa7
Frank Mayer <frank.mayer19072001@outlook.de>
2021-03-07 01:53:12 +0100

    Added default to when

--- HelperFunctions/when.ts
+++ HelperFunctions/when.ts
@@ -1,16 +1,21 @@
 function when<T, C>(
   condition: C,
-  ...cases: Array<{ if: C | (() => C); then: (() => T | null) | T }>
+  ...cases: Array<{ if: C | (() => C) | null; then: (() => T | null) | T }>
 ): T | null {
   if (typeof condition == "undefined" || condition == null) {
     for (const c of cases) {
-      if ((typeof c.if == "function" && (<() => C>c.if)()) || c.if) {
+      if (
+        c.if === null ||
+        (typeof c.if == "function" && (<() => C>c.if)()) ||
+        c.if
+      ) {
         return typeof c.then == "function" ? (<() => T>c.then)() : c.then;
       }
     }
   } else {
     for (const c of cases) {
       if (
+        c.if === null ||
         (typeof c.if == "function" && condition === (<() => C>c.if)()) ||
         condition === c.if
       ) {
